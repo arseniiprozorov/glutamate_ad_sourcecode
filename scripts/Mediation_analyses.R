a1e93777a253a33c7b185b812c3d9c2ffa232632
Arsenii Prozorov <161972649+arseniiprozorov@users.noreply.github.com>
2026-02-02 18:56:43 -0500

    Add combined mediation analyses and plotting
    Add a Hypothesis 2 workflow that builds a mediation_combined dataset (creates mean_structure, mean_activation, mean_glu composites), runs multiple mediation models (combined, parietal superior activation, hippocampal activation) and individual mediator analyses for precuneus and ACC using mediation::mediate, and plots/summarizes the results. Also call vignette("mediation"), add several cor.test/summary calls, adjust bootstrap sims from 5000 to 1000, and perform minor cleanup and plotting additions.

--- scripts/Mediation_analyses.R
+++ scripts/Mediation_analyses.R
@@ -2,7 +2,7 @@
 
 install.packages("mediation")
 library(mediation)
-
+vignette("mediation")
 names(MRS_full)
 
 # Precuneus Dataset
@@ -26,7 +26,6 @@ cor.test(MRS_Prec$cortical_thickness_adsignature_dickson,MRS_Prec$hipp_mean)
 cor.test(MRS_Prec$hipp_mean_act,MRS_Prec$activation_parietal_sup_l)
 
 
-
 # Create a temporary subset of just the two columns
 cols_struc_prec <- MRS_Prec[, c("cortical_thickness_adsignature_dickson", "hipp_mean")]
 cols_struc_acc  <- MRS_ACC[, c("cortical_thickness_adsignature_dickson", "hipp_mean")]
@@ -38,6 +37,9 @@ cols_act_prec <- MRS_Prec[, c("hipp_mean_act", "activation_parietal_sup_l")]
 cols_act_acc  <- MRS_ACC[, c("hipp_mean_act", "activation_parietal_sup_l")]
 MRS_Prec$mean_activation <- rowMeans(scale(cols_act_prec), na.rm = TRUE)
 MRS_ACC$mean_activation  <- rowMeans(scale(cols_act_acc), na.rm = TRUE)
+
+MRS_full <- MRS_Prec$mean_activation 
+MRS_full <- MRS_ACC$mean_structure
 
 
 # Model 1 structure --> Glu (precuneus) --> activation
@@ -51,7 +53,8 @@ summary(mediation_act_struc)
 #  mediate
 mediation_stru_prec_act <- mediate(mediation_struc_prec, mediation_act_struc, 
   treat = "mean_structure", mediator = "m_m_precuneus", 
-  boot = TRUE, sims = 5000)
+  boot = TRUE, sims = 1000)
+plot(mediation_stru_prec_act)
 summary(mediation_stru_prec_act)
 
 
@@ -69,6 +72,153 @@ mediation_stru_acc_act <- mediate(mediation_struc_acc, mediation_act_struc_acc,
                                    boot = TRUE, sims = 5000)
 summary(mediation_stru_acc_act)
 
+
+
+
+
+######################### Hypothesis 2 - compensation vs excitotoxcitiy ############
+# 1. Select only the raw columns that actually exist in MRS_full
+mediation_combined <- MRS_full[, c("m_m_precuneus", "m_m_acc", "hipp_mean", 
+                                   "cortical_thickness_adsignature_dickson", 
+                                   "hipp_mean_act", "activation_parietal_sup_l", 
+                                   "sex", "age_spectro")] |> na.omit()
+
+# 2. Create the composite "mean" variables inside this dataset
+mediation_combined$mean_structure <- rowMeans(scale(mediation_combined[, c("hipp_mean", "cortical_thickness_adsignature_dickson")]), na.rm = TRUE)
+mediation_combined$mean_activation <- rowMeans(scale(mediation_combined[, c("hipp_mean_act", "activation_parietal_sup_l")]), na.rm = TRUE)
+mediation_combined$mean_glu <- rowMeans(scale(mediation_combined[, c("m_m_precuneus", "m_m_acc")]), na.rm = TRUE)
+
+cor.test(mediation_combined$m_m_precuneus,mediation_combined$m_m_acc)
+
+
+## 1. model combined
+#  M <- X
+mediation_struc_glu <- lm(mean_glu ~ mean_structure, data = mediation_combined)
+summary(mediation_struc_glu)
+#M Y˜X + M
+mediation_act_struc <- lm(mean_activation ~ mean_structure + mean_glu, data = mediation_combined)
+summary(mediation_act_struc)
+
+#  mediate
+mediation_stru_glu_act <- mediate(mediation_struc_glu, mediation_act_struc, 
+                                   treat = "mean_structure", mediator = "mean_glu", 
+                                   boot = TRUE, sims = 1000)
+plot(mediation_stru_glu_act)
+summary(mediation_stru_glu_act)
+
+
+
+## 2 model parietal superior activaiton
+#  M <- X
+mediation_struc_glu <- lm(mean_glu ~ mean_structure, data = mediation_combined)
+summary(mediation_struc_glu)
+#M Y˜X + M
+mediation_act_struc <- lm(activation_parietal_sup_l ~ mean_structure + mean_glu, data = mediation_combined)
+summary(mediation_act_struc)
+
+#  mediate
+mediation_stru_glu_act <- mediate(mediation_struc_glu, mediation_act_struc, 
+                                  treat = "mean_structure", mediator = "mean_glu", 
+                                  boot = TRUE, sims = 1000)
+plot(mediation_stru_glu_act)
+summary(mediation_stru_glu_act)
+
+
+## 3 model hippocmapal activation
+#  M <- X
+mediation_struc_glu <- lm(mean_glu ~ mean_structure, data = mediation_combined)
+summary(mediation_struc_glu)
+#M Y˜X + M
+mediation_act_struc <- lm(hipp_mean_act ~ mean_structure + mean_glu, data = mediation_combined)
+summary(mediation_act_struc)
+
+#  mediate
+mediation_stru_glu_act <- mediate(mediation_struc_glu, mediation_act_struc, 
+                                  treat = "mean_structure", mediator = "mean_glu", 
+                                  boot = TRUE, sims = 1000)
+plot(mediation_stru_glu_act)
+summary(mediation_stru_glu_act)
+
+
+
+
+############# 4 models #########
+# M <- X
+m1_med <- lm(m_m_precuneus ~ mean_structure, data = mediation_combined)
+# Y ~ X + M
+m1_out <- lm(activation_parietal_sup_l ~ mean_structure + m_m_precuneus, data = mediation_combined)
+
+# Mediate
+med_prec_parietal <- mediate(m1_med, m1_out, 
+                             treat = "mean_structure", mediator = "m_m_precuneus", 
+                             boot = TRUE, sims = 1000)
+summary(med_prec_parietal)
+plot(med_prec_parietal)
+
+
+# M <- X
+m1_med <- lm(m_m_precuneus ~ mean_structure, data = mediation_combined)
+# Y ~ X + M
+m1_out <- lm(activation_parietal_sup_l ~ mean_structure + m_m_precuneus, data = mediation_combined)
+
+# Mediate
+med_prec_parietal <- mediate(m1_med, m1_out, 
+                             treat = "mean_structure", mediator = "m_m_precuneus", 
+                             boot = TRUE, sims = 1000)
+summary(med_prec_parietal)
+plot(med_prec_parietal)
+
+
+
+
+
+
+# M <- X
+m3_med <- lm(m_m_acc ~ mean_structure, data = mediation_combined)
+# Y ~ X + M
+m3_out <- lm(activation_parietal_sup_l ~ mean_structure + m_m_acc, data = mediation_combined)
+
+# Mediate
+med_acc_parietal <- mediate(m3_med, m3_out, 
+                            treat = "mean_structure", mediator = "m_m_acc", 
+                            boot = TRUE, sims = 1000)
+summary(med_acc_parietal)
+plot(med_acc_parietal)
+
+
+
+# M <- X
+m4_med <- lm(m_m_acc ~ mean_structure, data = mediation_combined)
+# Y ~ X + M
+m4_out <- lm(hipp_mean_act ~ mean_structure + m_m_acc, data = mediation_combined)
+
+# Mediate
+med_acc_hipp <- mediate(m4_med, m4_out, 
+                        treat = "mean_structure", mediator = "m_m_acc", 
+                        boot = TRUE, sims = 1000)
+summary(med_acc_hipp)
+plot(med_acc_hipp)
+
+
+
+
+# Model M: Does Structure predict Precuneus Glu?
+m_prec_med <- lm(m_m_precuneus ~ mean_structure, data = mediation_combined)
+
+# Model Y: Do Structure + Precuneus Glu predict Hippocampal Activation?
+m_prec_out <- lm(hipp_mean_act ~ mean_structure + m_m_precuneus, data = mediation_combined)
+
+# Run Mediation
+med_prec_hipp <- mediate(m_prec_med, m_prec_out, 
+                         treat = "mean_structure", mediator = "m_m_precuneus", 
+                         boot = TRUE, sims = 1000)
+
+
+summary(med_prec_hipp)
+
+
+
+# other hypotheses
 
 # Model 3 Prec --> act --> stru
 # M <- X (Does Glu predict Activation?)
