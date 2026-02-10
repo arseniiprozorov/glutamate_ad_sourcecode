c42ff88aec396d749758e0de16393f33980ee9ed
Arsenii Prozorov <161972649+arseniiprozorov@users.noreply.github.com>
2026-02-10 09:36:34 -0500

    Add WRS2 ZYmediate

--- scripts/Mediation_analyses.R
+++ scripts/Mediation_analyses.R
@@ -38,20 +38,9 @@ summary(med_comb)
 plot(med_comb)
 
 
-
-install.packages("robmed")
+#### With robmed package ####
 library(robmed)
 
-#fit_mediation(object,x,y,m,covariates = NULL,method = c("regression", "covariance"),robust = TRUE,
-#family = "gaussian",model = c("parallel", "serial"),contrast = FALSE,fit_yx = TRUE,control = NULL,)
-
-library(robmed)
-
-# 1. Fit the robust mediation model
-# We map your variables: 
-# X (Treat) = mean_structure
-# M (Mediator) = mean_glu
-# Y (Outcome) = mean_activation
 
 rob_mediation <- fit_mediation(mediation_combined,
   x = "mean_structure",
@@ -59,10 +48,19 @@ rob_mediation <- fit_mediation(mediation_combined,
   m = "mean_glu")
 summary(rob_mediation)
 
-# 3. Test for significance (Indirect Effect)
-# This replaces the 'boot = TRUE' step from the previous package.
 rob_test <- test_mediation(rob_mediation)
 summary(rob_test)
+
+
+
+######## With WRS2 package ##########
+library(WRS2)
+x <- mediation_combined$mean_structure
+y <- mediation_combined$mean_activation
+med <- mediation_combined$mean_glu
+zmediate_results <- ZYmediate(x, y, med, nboot = 5000, alpha = 0.05, kappa = 0.05)
+zmediate_results
+
 
 
 
